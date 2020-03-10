75176b8e0e7d8ecaabf2e0b0092c737febd98301
Alexandre Carvalho <alexandrecarvalho@MBPdeAlexandre.home>
2020-03-10 22:26:19 +0000

    server config to heroku, port listenner

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.put('/image', imageEntries.handleImageEntries(db));
 app.post('/imageurl', imageEntries.handleApiCall);
 
 app.listen(process.env.PORT || 3000, ()=>{
-	console.log(Server running on port ${process.env.PORT}`)
+	console.log(`Server running on port ${process.env.PORT}`)
 })
 
 
